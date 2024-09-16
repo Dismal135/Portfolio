fc57c423573e4934469ad31ee2fcb10da5012d72
PHYO THI HA <phyothiha97531@gmail.com>
2024-09-17 01:12:14 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <>
+      <header>
+        <h1>Portfolio</h1>
+        <nav id="navbar">
+          <a href={"#welcome-section"}>About</a>
+          <a href={"#project"}>Projects</a>
+          <a href={"#footer"}>Contact</a>
+        </nav>
       </header>
-    </div>
+  <main>
+    <section id="welcome-section">
+      <h1>Hi! I am Phyo Thiha <span>a Front end web developer.</span></h1>
+    </section>
+
+    <section id="projects">
+      <h1>These are some of my projects</h1>
+      <div className="projects">
+        <div className="project">
+          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="#"/>
+            <div className="project-tile">lorem ipsum</div>
+        </div>
+        <div className="project">
+          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png" alt="#"/>
+            <div className="project-tile">lorem ipsum</div>
+        </div>
+        <div className="project">
+          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png" alt="#"/>
+            <div className="project-tile">lorem ipsum</div>
+        </div>
+        <div className="project">
+          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/map.jpg" alt="#"/>
+            <div className="project-tile">lorem ipsum</div>
+        </div>
+      </div>
+      <a href={"#footer"} id="show">Show All <span>></span></a>
+    </section>
+  </main>
+  <footer id="footer">
+    <section id="upper">
+      <div id="description">
+        <h1>Let's work together...</h1>
+        <p>Do you like coffee?</p>
+      </div>
+      <div id="icons">
+        <div className="icon">
+          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
+        </div>
+        <div>
+          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
+        </div>
+        <div>
+          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
+        </div>
+        <div>
+          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
+        </div>
+      </div>
+    </section>
+
+    <section id="bottom">
+      <p>© Created for freeCodeCamp</p>
+    </section>
+  </footer>
+  </>
   );
 }
 
