7fbbc2cec1f444ca49c11943fb832cc35250a2eb
unknown <phyothiha97531@gmail.com>
2024-09-20 07:24:06 +0630

    update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,60 @@
 import './App.css';
 
 function App() {
-  return (
-      <>
-      <header>
-        <h1>Portfolio</h1>
-        <nav id="navbar">
-          <a href={"#welcome-section"}>About</a>
-          <a href={"#project"}>Projects</a>
-          <a href={"#footer"}>Contact</a>
-        </nav>
-      </header>
-  <main>
-    <section id="welcome-section">
-      <h1>Hi! I am Phyo Thiha <span>a Front end web developer.</span></h1>
-    </section>
+    return (<>
+        <header>
+            <h1>Portfolio</h1>
+            <nav>
+                <a href={"#hero"}>About</a>
+                <a href={"#project"}>Projects</a>
+                <a href={"#contact"}>Contact</a>
+            </nav>
+        </header>
+        <main>
+            <section id="hero">
+                <div className="avatar"></div>
+                <h1>Hi! I am PhyoThiha a Front end web developer.<a className={"readmore-button"} href={"#hero"}>Read
+                    More...</a></h1>
+                <div className={'wrapper'}>
+                    <h1>Skills<p>Html, Css, JavaScript, ReactJs, JavaScript Libraries, PHP</p></h1>
+                    <h1>Experience<p>MyanmarMediaLinkAge(1year)</p></h1>
+                </div>
+                <div className={'wrapper'}>
+                    <h1>From<p>Yangon,Thaketa</p></h1>
+                    <h1>BirthDate<p>24/11(Novemember)/1996</p></h1>
+                    <h1>Graduate From<p>No.5 Education High School Botahtaung</p></h1>
+                    <h1>Education<p>Civil-Engineering (Thanlyin Technology University), Dagon University English Major
+                        Distance</p></h1>
+                </div>
+            </section>
 
-    <section id="projects">
-      <h1>These are some of my projects</h1>
-      <div className="projects">
-        <div className="project">
-          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="#"/>
-            <div className="project-tile">lorem ipsum</div>
-        </div>
-        <div className="project">
-          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png" alt="#"/>
-            <div className="project-tile">lorem ipsum</div>
-        </div>
-        <div className="project">
-          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/calc.png" alt="#"/>
-            <div className="project-tile">lorem ipsum</div>
-        </div>
-        <div className="project">
-          <img className="project-img" src="https://cdn.freecodecamp.org/testable-projects-fcc/images/map.jpg" alt="#"/>
-            <div className="project-tile">lorem ipsum</div>
-        </div>
-      </div>
-      <a href={"#footer"} id="show">Show All <span>></span></a>
-    </section>
-  </main>
-  <footer id="footer">
-    <section id="upper">
-      <div id="description">
-        <h1>Let's work together...</h1>
-        <p>Do you like coffee?</p>
-      </div>
-      <div id="icons">
-        <div className="icon">
-          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
-        </div>
-        <div>
-          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
-        </div>
-        <div>
-          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
-        </div>
-        <div>
-          <a href={"#footer"} className="fa fa-facebook"><span>Facebook</span></a>
-        </div>
-      </div>
-    </section>
-
-    <section id="bottom">
-      <p>© Created for freeCodeCamp</p>
-    </section>
-  </footer>
-  </>
-  );
+            <section id={"project"}>
+                <h1>My Projects</h1>
+                <div className={'wrapper'}>
+                    <a className={"project-link"} href={"https://randomquotemachinephyothiha.netlify.app"}
+                       target={'_blank'} rel={'noreferrer'}>
+                        <img className={'project-img'} src={'/images/quotemachine.png'} alt={"quotemachine"}/>
+                        <h1 className={'project-title'}>Random Quote Machine</h1>
+                    </a>
+                </div>
+                <div className={'wrapper'}>
+                    <a className={"project-link"} href={"https://reacttechnicaldocumentation.netlify.app"}
+                       target={'_blank'} rel={'noreferrer'}>
+                        <img className={'project-img'} src={'/images/technicaldoc.png'} alt={"technicaldocument"}/>
+                        <h1 className={'project-title'}>Technical Documentation</h1>
+                    </a>
+                </div>
+                <div className={'wrapper'}>
+                    <img className={'project-img'} src={'/images/quotemachine.png'} alt={"quotemachine"}/>
+                    <h1 className={'project-title'}>Random Quote Machine</h1>
+                </div>
+                <div className={'wrapper'}>
+                    <img className={'project-img'} src={'/images/quotemachine.png'} alt={"quotemachine"}/>
+                    <h1 className={'project-title'}>Random Quote Machine</h1>
+                </div>
+            </section>
+        </main>
+    </>);
 }
 
 export default App;
